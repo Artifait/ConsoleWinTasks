01052efe110522940ce37ecfacd21c82472c82ed
top_acdemy_student <top_acdemy_student@CODE-02>
2024-09-30 16:45:17 +0700

    Fix

---
+++ UI/Win/ApplicationWin/Task1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWinTasks.UI.Win.ApplicationWin
+{
+    internal class Task1
+    {
+    }
+}
